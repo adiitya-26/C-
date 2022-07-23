5a9b3235103a64847869c4d6d53a8c0952146c17
Aditya <109268100+adiitya-26@users.noreply.github.com>
2022-07-23 10:43:28 +0530

    Add files via upload

---
+++ nested.cc
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+int main()
+
+/* Program to find an integer is positive,
+   negative or zero. Nested if...else statement */
+{
+    int num;
+    cout <<"Enter an integer: ";
+    cin >> num;
+  // outer if condition
+    if(num != 0)
+
+  // inner if condition
+    if(num > 0)
+    {
+      cout <<"The number is positive" << endl;
+    }
+
+  // inner else condition
+    else
+    {
+      cout <<"The number is negative" << endl;
+    }
+
+  // outer else condition
+    else
+    {
+      cout <<"The number 0 is neither positive nor negative." << endl;
+    }
+    cout <<"This line is always printed" << endl;
+
+    return 0;
+
+
+  }
