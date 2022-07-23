43cd6637ec1a8ce08289c6720e919a120b1e45fa
Aditya <109268100+adiitya-26@users.noreply.github.com>
2022-07-23 10:40:33 +0530

    Add files via upload

---
+++ evenornot.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main()
+
+//program to check nos. is even or not
+{
+    int n;
+    cout <<"Enter an Integer: ";
+    cin >>n;
+
+    if(n%2 == 0)
+    {
+        cout <<n <<"  is even";
+    }
+    else
+    {
+        cout <<n <<"  is odd";
+    }
+     return 0;
+}
