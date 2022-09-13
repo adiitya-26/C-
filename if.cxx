c07334f50640b8c2ce04c16704384f9fff56d214
Aditya <109268100+adiitya-26@users.noreply.github.com>
2022-09-13 23:15:30 +0530

    Add files via upload

---
+++ if.cxx
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+ // using if statement
+
+{
+   int age;
+   cout <<"Your age is: ";
+   cin >> age;
+
+   if( age < 18)
+   {
+     cout <<"You are under age";
+   }
+   if( age >= 18)
+   {
+     cout <<"You are not under age";}
+   }
